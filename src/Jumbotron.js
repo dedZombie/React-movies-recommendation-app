8439e19144ae682e77fafcd3f930405e4500dcc4
dedZombie <hemicar92@gmail.com>
2018-11-27 19:51:47 +0100

    Added some background-color and fixed jumbotron

--- src/Jumbotron.js
+++ src/Jumbotron.js
@@ -3,9 +3,9 @@ import React, { Component } from 'react'
 export class Jumbotron extends Component {
   render() {
     return (
-        <div className="jumbotron">
-            <h1 className="display-4">Hello, world!</h1>
-            <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <div className="jumbotron" style={{'backgroundColor': 'rgba(102, 16, 242, 0.3)'}}>
+            <h1 className="display-1 text-white">Welcome to Movies</h1>
+            <p className="lead text-white">This is a place where you can bookmark all the movies you want to watch and rate them. Enjoy!</p>
         </div>
     )
   }
