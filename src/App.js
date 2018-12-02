91466810109a51f40c7f7973b26f65b66e0b6b18
dedZombie <hemicar92@gmail.com>
2018-12-02 17:29:56 +0100

    Switched to TMDB API, fixed main page

--- src/App.js
+++ src/App.js
@@ -2,26 +2,44 @@ import React, { Component } from 'react';
 import Navbar from './Navbar';
 import Jumbotron from './Jumbotron';
 
-const apiKey = 'https://api.airtable.com/v0/appyKRn8VsOzsHhNK/Favorites?api_key=keyGCmyUxgakOzL0a';
+const apiKey = '97a1e2899ef7d9f4d720ed98d1bf26bb';
+const baseURL = 'https://api.themoviedb.org/3/movie'
+const imageURL = 'https://image.tmdb.org/t/p/'
+// const configuration = `https://api.themoviedb.org/3/configuration?api_key=${apiKey}`;
+// const id = Math.random() * 100;
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      movies: []
+      movies: [],
     }
   }
+  
 
   componentDidMount() {
-    fetch(apiKey)
+    fetch(`${baseURL}/popular?api_key=${apiKey}`)
     .then(res => res.json())
     .then(data => {
-      this.setState({ movies: data.records });
+      console.log(data);
+      this.setState({ movies: data.results });
     }).catch(err => {
       // Error
-      console.err(err);
+      console.error(err);
     });
+
+    // fetch(configuration)
+    // .then(res => res.json())
+    // .then(data => {
+    //   console.log(data);
+    //   const baseURL = data.images.base_url;
+    //   const posterSize = data.images.poster_sizes[4];
+    //   const imageURL = baseURL + posterSize + this.posterPath;
+    //   console.log('Acquired imageURL: ' + imageURL);
+    // }).catch(err => {
+    //   console.error(err);
+    // });
   }
 
   render() {
@@ -33,7 +51,7 @@ class App extends Component {
           <div className="row">
             <div className="col">
               <div className="card-columns">
-                {this.state.movies.map(movie => <MovieCard {...movie.fields} /> )}
+                {this.state.movies.map(movie => <MovieCard {...movie} /> )}
               </div>
             </div>
           </div>
@@ -43,14 +61,14 @@ class App extends Component {
   }
 }
 
-const MovieCard = ({ title, year, description, imageURL }) => (
-  <div className="card">
-    <img src={imageURL[0].url} alt="Movie poseter" className="card-img-top"/>
+const MovieCard = ({ title, overview, poster_path, release_date, id }) => (
+  <div className="card" key={id}>
+    <img src={imageURL+'w500'+ poster_path} alt="Movie poster" className="card-img-top"/>
     <div className="card-body">
       <h5 className="card-title">{title}</h5>
-      <p className="card-text">{description}</p>
+      <p className="card-text">{overview}</p>
       <p className="card-footer">
-        <small className="text-muted">{year}</small>
+        <small className="text-muted">{release_date}</small>
       </p>
     </div>
   </div>
